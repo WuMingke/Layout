b1ded00aa4efb3447d3d283dcb2c91fc2f6071f7
wumingke <766190553@qq.com>
2019-11-06 16:09:00 +0800

    angle

--- constraintlayout/src/main/java/com/erkuai/constraintlayout/MainActivity.java
+++ constraintlayout/src/main/java/com/erkuai/constraintlayout/MainActivity.java
@@ -1,14 +1,63 @@
 package com.erkuai.constraintlayout;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
+import android.animation.ValueAnimator;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.LinearInterpolator;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // setContentView(R.layout.activity_center);
+        //setContentView(R.layout.activity_weight);
+        //setContentView(R.layout.activity_baseline);
+        setContentView(R.layout.activity_angle);
+        View sun = findViewById(R.id.view1);
+        final View earth = findViewById(R.id.view2);
+        final View moon = findViewById(R.id.view3);
+
+        final ValueAnimator animator = ValueAnimator.ofFloat(0, 1);
+        animator.setDuration(10000);
+        animator.setInterpolator(new LinearInterpolator());
+        animator.setRepeatCount(ValueAnimator.INFINITE);
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams
+                        ) earth.getLayoutParams();
+                layoutParams.circleAngle = 45 + animation.getAnimatedFraction() * 360;
+                earth.requestLayout();
+            }
+        });
+
+        final ValueAnimator animator2 = ValueAnimator.ofFloat(0, 1);
+        animator2.setDuration(2000);
+        animator2.setInterpolator(new LinearInterpolator());
+        animator2.setRepeatCount(ValueAnimator.INFINITE);
+        animator2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                ConstraintLayout.LayoutParams layoutParams = (ConstraintLayout.LayoutParams
+                        ) moon.getLayoutParams();
+                layoutParams.circleAngle = 270 + animation.getAnimatedFraction() * 360;
+                moon.requestLayout();
+            }
+        });
+
+        sun.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                animator.start();
+                animator2.start();
+            }
+        });
+
+
     }
 }
