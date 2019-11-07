a36f37c0bf59d2f9c44c9bb5da9433dff6e998be
wumingke <766190553@qq.com>
2019-11-07 10:38:52 +0800

    motionLayout

--- motionlayout/src/main/java/com/erkuai/motionlayout/MainActivity.java
+++ motionlayout/src/main/java/com/erkuai/motionlayout/MainActivity.java
@@ -3,12 +3,32 @@ package com.erkuai.motionlayout;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.transition.Scene;
+import android.transition.TransitionManager;
+import android.view.Gravity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 public class MainActivity extends AppCompatActivity {
+
+    private View view;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        //setContentView(R.layout.activity_translate);
+
+        //view = findViewById(R.id.view);
+
+        setContentView(R.layout.activity_motion);
+    }
+
+    public void translate(View view) {
+        TransitionManager.beginDelayedTransition((ViewGroup) view.getParent());
+        FrameLayout.LayoutParams params = (FrameLayout.LayoutParams) view.getLayoutParams();
+        params.gravity = Gravity.END;
+        view.setLayoutParams(params);
+
     }
 }
